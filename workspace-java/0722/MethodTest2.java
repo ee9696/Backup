28465e840f1ba916ae91c0e7560c9a361fbb132c
ee9696 <93972810+ee9696@users.noreply.github.com>
2022-02-17 17:58:32 +0900

    Update MethodTest2.java

--- workspace-java/0722/MethodTest2.java
+++ workspace-java/0722/MethodTest2.java
@@ -4,10 +4,10 @@ class MethodTest2 {
       
       MethodTest2 mt = new MethodTest2();//생성
 
-      System.out.println("ÇÕ = " + mt.sum(320, 258)); //호출
-      System.out.println("Â÷ = " + mt.sub(320, 258)); //62
-	  System.out.println("°ö = " + mt.mul(320, 258)); //
-	  System.out.println("¸ò = " + String.format("%.2f",mt.div(320, 258))); //1.24
+      System.out.println("합 = " + mt.sum(320, 258)); //호출
+      System.out.println("차 = " + mt.sub(320, 258)); //62
+	  System.out.println("곱 = " + mt.mul(320, 258)); //
+	  System.out.println("몫 = " + String.format("%.2f",mt.div(320, 258))); //1.24
    }
 
    public double div(int a, int b){
@@ -22,7 +22,7 @@ class MethodTest2 {
 	   return a-b;
    }
 
-   public int sum(int a, int b) { //±¸Çö
+   public int sum(int a, int b) { //구현
       return a+b;
    }
 }
