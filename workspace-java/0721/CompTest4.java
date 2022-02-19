146696d4afdae663e59b14736f9b082a4e2cf1ec
ee9696 <93972810+ee9696@users.noreply.github.com>
2022-02-19 20:59:13 +0900

    Update CompTest4.java

--- workspace-java/0721/CompTest4.java
+++ workspace-java/0721/CompTest4.java
@@ -3,14 +3,14 @@ class CompTest4 {
 		char ch = 'z';
 
 		char result = ch>='A' && ch<='Z' ? (char)(ch + 32) : (char)(ch - 32);
-		System.out.println(ch + " ¡æ " + result);
+		System.out.println(ch + " â†’ " + result);
 		// int result = ch>='A' && ch<='Z' ? ch+32 : ch-32;
-		// Ssystem.out.println(ch + " ¡æ " + (char)result);
+		// Ssystem.out.println(ch + " â†’ " + (char)result);
 
 	}
 }
 
 
-[½ÇÇà°á°ú]
+[Â½Ã‡Ã‡Ã Â°Ã¡Â°Ãº]
 
-z ¡æ Z
+z Â¡Ã¦ Z
