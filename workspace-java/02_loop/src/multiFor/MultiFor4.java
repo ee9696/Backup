6abe8bfbce46e03514c6b44d627091ab1bd3f31b
ee9696 <93972810+ee9696@users.noreply.github.com>
2022-02-20 00:16:42 +0900

    Update MultiFor4.java

--- workspace-java/02_loop/src/multiFor/MultiFor4.java
+++ workspace-java/02_loop/src/multiFor/MultiFor4.java
@@ -6,7 +6,7 @@ public class MultiFor4 {
 		// TODO Auto-generated method stub
 		int dan, i;
 	    
-		for(int w=2; w<=8; w+=3) { //3¹ø w=1, w=2, w=3 -> w=2. w=5, w=8
+		for(int w=2; w<=8; w+=3) { //3ë²ˆ w=1, w=2, w=3 -> w=2. w=5, w=8
 			for(i=1; i<=9; i++) {      
 				for(dan=w; dan<=w+2; dan++) {
 					if(dan !=10)
@@ -30,7 +30,7 @@ public class MultiFor4 {
    public static void main(String[] args) {
       int dan, i;
       
-      for(int w=2; w<=8; w+=3) {//3¹ø w=1, w=2, w=3 -> w=2, w=5, w=8
+      for(int w=2; w<=8; w+=3) {//3ë²ˆ w=1, w=2, w=3 -> w=2, w=5, w=8
          for(i=1; i<=9; i++) {
             for(dan=w; dan<=w+2; dan++) {
                if(dan != 10)
